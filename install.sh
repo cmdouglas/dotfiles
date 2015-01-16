597514d34e4ccfcfe4d0861218223a612597840d
Charlie <cmdouglas@gmail.com>
2015-01-16 17:18:57 -0600

    edit install

--- install.sh
+++ install.sh
@@ -7,29 +7,12 @@ git submodule update --init
 
 echo 'linking stuff'
 local here=$(dirname $0)
-for file in .XCompose .ackrc .gitconfig .psqlrc .screenrc .tmux.conf .vimrc .vim .zshenv .zshrc; do
-    if [[ $file == '.gitconfig' && $USER != 'eevee' && $USER != 'amunroe' ]]; then
-        echo "not linking $file, it has my name in it!  do it yourself"
-    else
-        if [[ $(readlink -f $HOME/$file) != $(readlink -f $here/$file) ]]; then
-            ln -i -s -T $here/$file $HOME/$file
+for file in .gitconfig .psqlrc .screenrc .tmux.conf .vimrc .vim .zshenv .zshrc; do
+    if [[ $(readlink -f $HOME/$file) != $(readlink -f $here/$file) ]]; then
+        ln -i -s -T $here/$file $HOME/$file
             echo "linked $file"
-        fi
     fi
 done
-
-# airline (powerline) font stuff
-if whence fc-cache > /dev/null; then
-    echo 'installing powerline font'
-    mkdir -p ~/.fonts/
-    wget -O ~/.fonts/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
-    fc-cache -vf ~/.fonts/
-
-    mkdir -p ~/.config/fontconfig/conf.d/
-    wget -O ~/.config/fontconfig/conf.d/10-powerline-symbols.conf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-else
-    echo 'no fontconfig found; skipping powerline font'
-fi
 
 # install flake8 with default python
 if whence pip > /dev/null; then
